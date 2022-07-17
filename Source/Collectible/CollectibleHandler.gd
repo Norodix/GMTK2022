e14019326197cb8650efa850fed6243aabfc3c44
Norodix <peceli.bence@gmail.com>
2022-07-17 16:09:54 +0200

    Collectible see through walls

--- Source/Collectible/CollectibleHandler.gd
+++ Source/Collectible/CollectibleHandler.gd
@@ -1,7 +1,6 @@
 extends Node
 
 onready var collectibleScene = preload("Collectible.tscn")
-onready var canvasLayer = CanvasLayer.new()
 var pickupScore = 0
 
 onready var player :AudioStreamPlayer = AudioStreamPlayer.new()
@@ -9,15 +8,12 @@ onready var music = preload("res://Sound/sfx/success.wav")
 
 
 func _ready():
-	
-	self.add_child(canvasLayer)
-	
 	yield(get_tree().create_timer(0.5), "timeout")
 	var positions = get_tree().get_nodes_in_group("CollectiblePositions")
 	
 	for position in positions:
 		var c : Spatial = collectibleScene.instance()
-		canvasLayer.add_child(c)
+		self.add_child(c)
 		c.global_transform.origin = position.global_transform.origin
 		c.connect("pickup", self, "pickupCallback")
 		
