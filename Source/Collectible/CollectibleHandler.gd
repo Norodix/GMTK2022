d39e897e264e21831cb66e7c2d42f9c798269ba4
Norodix <peceli.bence@gmail.com>
2022-07-17 19:44:41 +0200

    Initialize collectibles bug fixed

--- Source/Collectible/CollectibleHandler.gd
+++ Source/Collectible/CollectibleHandler.gd
@@ -4,11 +4,20 @@ onready var collectibleScene = preload("Collectible.tscn")
 var pickupScore = 0
 var maxScore = 0
 
+var initialized = false
+
 onready var player :AudioStreamPlayer = AudioStreamPlayer.new()
 onready var music = preload("res://Sound/sfx/success.wav")
 
 
 func _ready():
+	pass
+
+
+func initialize_collectibles():
+	if initialized:
+		return
+	initialized = true
 	yield(get_tree().create_timer(0.5), "timeout")
 	var positions = get_tree().get_nodes_in_group("CollectiblePositions")
 	
@@ -23,7 +32,6 @@ func _ready():
 	# Create pickup effect player
 	get_tree().get_root().call_deferred("add_child", player)
 	player.set_stream(music)
-	
 
 
 func pickupCallback(caller):
---
+++ Source/World.gd
@@ -0,0 +1,5 @@
+extends Spatial
+
+func _ready():
+	CollectibleHandler.initialize_collectibles()
+	pass # Replace with function body.
